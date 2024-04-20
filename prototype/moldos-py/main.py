7d8f3feeb12ee0d165832b8e394a57824f670a58
MH <maytanan.h@gmail.com>
2024-04-20 15:33:17 +0700

    removed holistic, used hands + pose

--- prototype/moldos-py/main.py
+++ prototype/moldos-py/main.py
@@ -1,7 +1,9 @@
 import mediapipe as mp
 import cv2
 mp_drawing = mp.solutions.drawing_utils
-mp_holistic = mp.solutions.holistic
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_hands = mp.solutions.hands
+mp_pose = mp.solutions.pose
 from mediapipe.python.solutions.pose import PoseLandmark
 from mediapipe.python.solutions.hands import HandLandmark
 from tools import *
@@ -12,100 +14,116 @@ cap = cv2.VideoCapture(0)
 falsebuffer = 0
 active = False
 
-with mp_holistic.Holistic(min_detection_confidence=0.45, min_tracking_confidence=0.5) as holistic:
+hands = mp_hands.Hands(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=1)
+pose = mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5, model_complexity=1)
     
-    while cap.isOpened():
-        ret, frame = cap.read()
-        
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = holistic.process(image)
-        
+while cap.isOpened():
+    frame = cv2.flip(cap.read()[1], 1)
+  
+    image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    hands_results = hands.process(image)
+    pose_results = pose.process(cv2.flip(image, 1))
+    
+    try:
+        poselandmarks = pose_results.pose_landmarks.landmark
+
+        pose_pos = []
+        lhand_pos = []
+        rhand_pos = []
+        myHands = []
+        handsType = []
         try:
-            poselandmarks = results.pose_landmarks.landmark
-            try:
-                rhandlandmarks = results.right_hand_landmarks.landmark
-            except Exception as e:
-                # print(e)
-                pass
-            try:
-                lhandlandmarks = results.left_hand_landmarks.landmark
-            except Exception as e:
-                # print(e)
-                pass
+            for hand in hands_results.multi_handedness:
+                handType=hand.classification[0].label
+                handsType.append(handType)
+            for handLandMarks in hands_results.multi_hand_landmarks:
+                myHand = []
+                for landMark in handLandMarks.landmark:
+                    myHand.append(translatepos(((landMark.x), (landMark.y))))
+                myHands.append(myHand)
             
-            pose_pos = []
-            lhand_pos = []
-            rhand_pos = []
-            
-            for i in PoseLandmark:
-                try:
-                    pose_pos.append(translatepos(get_pos(poselandmarks, i)))
-                except Exception as e:
-                    print(e)
-                    pose_pos.append("N/A")
-            
-            for i in HandLandmark:
-                try:
-                    rhand_pos.append(translatepos(get_pos(rhandlandmarks, i)))
-                    lhand_pos.append(translatepos(get_pos(lhandlandmarks, i)))
-                except:
-                    rhand_pos.append("N/A")
-                    lhand_pos.append("N/A")
-
-            if not active:
-                correctpos = False
-                color = (255, 0, 0)
-
             try:
-                e4r = exercises.check4r(pose_pos, rhand_pos, lhand_pos)
-                active = True
-                if e4r:
-                    falsebuffer = 0
-                    correctpos = True
-                    color = (0, 255, 0)
-                elif not e4r and falsebuffer <= 5:
-                    falsebuffer += 1
-                    correctpos = True
+                handsType[1]
+                rhand_pos = myHands[1]
+                lhand_pos = myHands[0]
+            except:
+                if handsType[0] == "Right" and myHands:
+                    rhand_pos = myHands[0]
+                    for i in HandLandmark:
+                        lhand_pos.append("N/A")
+                elif handsType[0] == "Left" and myHands:
+                    lhand_pos = myHands[0]
+                    for i in HandLandmark:
+                        rhand_pos.append("N/A")
                 else:
-                    correctpos = False
-                    color = (255, 0, 0)
-                    
-            except Exception as e:
-                # print(e)
-                pass
-            
-            image = cv2.flip(image, 1)
-            image = cv2.putText(image, str(correctpos), (40, 80), cv2.FONT_HERSHEY_SIMPLEX,  
-                   2, color, 3, cv2.LINE_AA)
-            image = cv2.flip(image, 1) 
-            
+                    for i in HandLandmark:
+                        rhand_pos.append("N/A")
+                        lhand_pos.append("N/A")
+                
         except Exception as e:
             # print(e)
             pass
+
+        for i in PoseLandmark:
+            try:
+                # 1280-0 -> 0-1280
+                x, y = translatepos(get_pos(poselandmarks, i))
+                pose_pos.append((1280 - x, y))
+
+            except Exception as e:
+                print(e)
+                pose_pos.append("N/A")
+
+        if not active:
+            correctpos = False
+            color = (255, 0, 0)
+
+        try:
+            e4r = exercises.check4r(pose_pos, rhand_pos, lhand_pos)
+            active = True
+            if e4r:
+                falsebuffer = 0
+                correctpos = True
+                color = (0, 255, 0)
+            elif not e4r and falsebuffer <= 5:
+                falsebuffer += 1
+                correctpos = True
+            else:
+                correctpos = False
+                color = (255, 0, 0)
                 
-        # Right hand
-        mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                  mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
-                                 )
+        except Exception as e:
+            # print(e)
+            pass
 
-        # Left Hand
-        mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
-                                  mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
-                                 )
-
-        # Pose
-        mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
-                                  mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
-                                  mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
-                                 )
+        image = cv2.putText(image, str(correctpos), (40, 80), cv2.FONT_HERSHEY_SIMPLEX,  
+                2, color, 3, cv2.LINE_AA)
         
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)                
-        cv2.imshow('Raw Webcam Feed', cv2.flip(image, 1))
+    except Exception as e:
+        # print(e)
+        pass
+            
+    if hands_results.multi_hand_landmarks:
+      for hand_landmarks in hands_results.multi_hand_landmarks:
+        mp_drawing.draw_landmarks(
+            image,
+            hand_landmarks,
+            mp_hands.HAND_CONNECTIONS,
+            mp_drawing_styles.get_default_hand_landmarks_style(),
+            mp_drawing_styles.get_default_hand_connections_style())
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-            break
+    image = cv2.flip(image, 1)
+    mp_drawing.draw_landmarks(image, pose_results.pose_landmarks, mp_pose.POSE_CONNECTIONS, 
+                                mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                                mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                                )
+    image = cv2.flip(image, 1)
+    
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)                
+    cv2.imshow('MOLDOS', image)
+
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+        break
 
 cap.release()
 cv2.destroyAllWindows()
--- prototype/moldos-py/tools.py
+++ prototype/moldos-py/tools.py
@@ -16,4 +16,15 @@ def calculate_angle(a,b,c):
     if angle >180.0:
         angle = 360-angle
         
-    return angle 
+    return angle
+
+def maprange(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
